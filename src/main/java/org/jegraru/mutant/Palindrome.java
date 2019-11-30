6000c9eeea157af777b815b5df7d038e25d37ca4
Pedro Javier Martos Velasco <pjmv1986@gmail.com>
2019-11-30 19:34:34 +0100

    Demo contents

--- src/main/java/org/jegraru/mutant/Palindrome.java
+++ src/main/java/org/jegraru/mutant/Palindrome.java
@@ -3,16 +3,12 @@ package org.jegraru.mutant;
 class Palindrome {
 
     boolean isPalindrome(String input) {
-        return _isPalindrome(input.trim());
-    }
-
-    private static boolean _isPalindrome(String input) {
-        if (input.length() <= 1) {
+        if (input.length() == 0) {
             return true;
         }
         char firstChar = input.charAt(0);
         char lastChar = input.charAt(input.length() - 1);
         String mid = input.substring(1, input.length() - 1);
-        return (firstChar == lastChar) && _isPalindrome(mid);
+        return (firstChar == lastChar) && isPalindrome(mid);
     }
 }
--- src/test/java/org/jegraru/mutant/PalindromeTest.java
+++ src/test/java/org/jegraru/mutant/PalindromeTest.java
@@ -2,7 +2,6 @@ package org.jegraru.mutant;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PalindromeTest {
@@ -14,66 +13,65 @@ class PalindromeTest {
         assertTrue(palindrome.isPalindrome("noon"));
     }
 
-    @Test
-    void givenAnEmptyString_whenIsPalindrome_thenItShouldReturnTrue() {
-        assertTrue(palindrome.isPalindrome(""));
-    }
-
-    @Test
-    void givenAStringWithNonMatchingStartAndEnding_whenIsPalindrome_thenItShouldReturnFalse() {
-        assertFalse(palindrome.isPalindrome("nool"));
-    }
-
-    @Test
-    void givenAStringWithNonMatchingCharactersInTheMiddle_whenIsPalindrome_thenItShouldReturnFalse() {
-        assertFalse(palindrome.isPalindrome("nain"));
-    }
-
-    @Test
-    void givenABlankString_whenIsPalindrome_thenItShouldReturnTrue() {
-        assertTrue(palindrome.isPalindrome(" "));
-    }
-
-    @Test
-    void givenAPalindromeWithAnOddLength_whenIsPalindrome_thenItShouldReturnTrue() {
-        assertTrue(palindrome.isPalindrome("nooon"));
-    }
-
-    @Test
-    void givenAPalindromeWithUnbalancedSpaceAtTheBeginning_whenIsPalindrome_thenItShouldReturnTrue() {
-        assertTrue(palindrome.isPalindrome(" noon"));
-    }
-
-    @Test
-    void givenAPalindromeWithUnbalancedSpaceAtTheEnd_whenIsPalindrome_thenItShouldReturnTrue() {
-        assertTrue(palindrome.isPalindrome("noon "));
-    }
-
-    @Test
-    void givenAPalindromeWithUnbalancedWhitespaceAtStartAndMiddleAndEnd_whenIsPalindrome_thenItShouldReturnFalse() {
-        assertFalse(palindrome.isPalindrome("n  oo n "));
-    }
-
-    /*
-    private static Object[][] isPalindromeParams() {
-        return new Object[][] {
-                // input       expected
-                {  "noon",     true     },
-                {  "",         true     },
-                {  "nool",     false    },
-                {  "nain",     false    },
-                {  " ",        true     },
-                {  "nooon",    true     },
-                {  " noon",    true     },
-                {  "noon ",    true     },
-                {  "n  oo n ", false    }
-        };
-    }
-
-    @ParameterizedTest
-    @MethodSource("isPalindromeParams")
-    void givenAnInputString_whenIsPalindrome_thenItShouldReturnTheExpectedResult(String input, boolean expected) {
-        assertEquals(expected, new Palindrome().isPalindrome(input));
-    }
-    */
+//    @Test
+//    void givenAnEmptyString_whenIsPalindrome_thenItShouldReturnTrue() {
+//        assertTrue(palindrome.isPalindrome(""));
+//    }
+//
+//    @Test
+//    void givenAStringWithNonMatchingStartAndEnding_whenIsPalindrome_thenItShouldReturnFalse() {
+//        assertFalse(palindrome.isPalindrome("nool"));
+//    }
+//
+//    @Test
+//    void givenAStringWithNonMatchingCharactersInTheMiddle_whenIsPalindrome_thenItShouldReturnFalse() {
+//        assertFalse(palindrome.isPalindrome("nain"));
+//    }
+//
+//    @Test
+//    void givenABlankString_whenIsPalindrome_thenItShouldReturnTrue() {
+//        assertTrue(palindrome.isPalindrome(" "));
+//    }
+//
+//    @Test
+//    void givenAPalindromeWithAnOddLength_whenIsPalindrome_thenItShouldReturnTrue() {
+//        assertTrue(palindrome.isPalindrome("nooon"));
+//    }
+//
+//    @Test
+//    void givenAPalindromeWithUnbalancedSpaceAtTheBeginning_whenIsPalindrome_thenItShouldReturnTrue() {
+//        assertTrue(palindrome.isPalindrome(" noon"));
+//    }
+//
+//    @Test
+//    void givenAPalindromeWithUnbalancedSpaceAtTheEnd_whenIsPalindrome_thenItShouldReturnTrue() {
+//        assertTrue(palindrome.isPalindrome("noon "));
+//    }
+//
+//    @Test
+//    void givenAPalindromeWithUnbalancedWhitespaceAtStartAndMiddleAndEnd_whenIsPalindrome_thenItShouldReturnFalse() {
+//        assertFalse(palindrome.isPalindrome("n  oo n "));
+//    }
+//
+//    private static Object[][] isPalindromeParams() {
+//        return new Object[][] {
+//                /* input       expected */
+//                {  "noon",     true      },
+//                {  "",         true      },
+//                {  "nool",     false     },
+//                {  "nain",     false     },
+//                {  " ",        true      },
+//                {  "nooon",    true      },
+//                {  " noon",    true      },
+//                {  "noon ",    true      },
+//                {  "n  oo n ", false     }
+//        };
+//    }
+//
+//    @ParameterizedTest
+//    @MethodSource("isPalindromeParams")
+//    void givenAnInputString_whenIsPalindrome_thenItShouldReturnTheExpectedResult(String input, boolean expected) {
+//        assertEquals(expected, new Palindrome().isPalindrome(input));
+//    }
+//
 }
